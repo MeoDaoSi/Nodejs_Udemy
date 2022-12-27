b0e1c79c3f89d5fb10f8903a9552585bdac7497a
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2022-12-27 17:12:27 +0700

    validator, chalk, global nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
+const validator = require('validator');
+const chalk = require('chalk');
 
-fs.writeFileSync('notes.txt','My name Tinh.');
-fs.appendFileSync('notes.txt',' Append new massage');
+console.log(chalk.inverse.bgRed.green('Success!'));
+// console.log(chalk.green(getNotes()));
+console.log(validator.isEmail('levant13213inh1234324234@gaicom'));
