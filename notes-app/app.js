0ef17a8f1dbb3ea12266e3418db9cdc635fb3fd8
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2022-12-29 19:47:12 +0700

    yargs agrv parse

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,53 @@
 const validator = require('validator');
-const chalk = require('chalk');
+const yargs = require('yargs');
+const chalk = require('chalk'); 
 
-console.log(chalk.inverse.bgRed.green('Success!'));
-// console.log(chalk.green(getNotes()));
-console.log(validator.isEmail('levant13213inh1234324234@gaicom'));
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: String,
+        },
+        body:{
+            describe: 'Note body',
+            demandOption: true,
+            type: String,
+        }
+    },
+    handler(argv){
+        console.log('Adding a new note!' , argv );
+    }
+})
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler(){
+        console.log('Reading a note!');
+    }
+})
+// Remove add command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler(){
+        console.log('Removing a new note!');
+    }
+})
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler(){
+        console.log('Listing out all note');
+    }
+})
+
+yargs.parse();
