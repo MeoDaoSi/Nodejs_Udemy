eb932b6fa527218324bb807fe7a952f9d9b3f976
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2023-01-07 12:06:08 +0700

    bug HTTP Strict Transport Security

---
+++ playground/4-callback.js
@@ -0,0 +1,34 @@
+// setTimeout( ()=> {
+//     console.log('hello world');
+// },2000)
+
+// const nameList = ['tinhle', 'abc', 'kimoanh', '1', '2']
+
+// const love = nameList.filter( name => name.length > 4 )
+
+// // console.log(love);
+
+// const geocode = (address, callback) => {
+//     setTimeout( ()=> {
+//         const data = {
+//             latitude: 0,
+//             longitude: 0
+//         }
+//         callback(data);
+//     },2000)
+// }
+
+// geocode('vietnam', (location) => {
+//     console.log(location);
+// });
+
+// const add = (a,b,callback) => {
+//     setTimeout( () => {
+//         const sum = a + b;
+//         callback(sum);
+//     },2000)
+// }
+
+// add(1,4, (sum) => {
+//     console.log(sum);
+// })
