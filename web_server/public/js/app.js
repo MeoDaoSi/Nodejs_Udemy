42787645bf3270b25206245a7b9a2c599cb5dac2
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2023-02-03 09:53:57 +0700

    weather complied

--- web_server/public/js/app.js
+++ web_server/public/js/app.js
@@ -1,9 +1,20 @@
 const formW = document.querySelector('.formWeather')
 const inputValue = document.querySelector('.inputWeather')
+const messageOne = document.querySelector('#message_1')
+const messageTwo = document.querySelector('#message_2')
 
 formW.addEventListener('submit' , (e) => {
     e.preventDefault();
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
     fetch(`http://localhost:3000/weather?address=${inputValue.value}`)
         .then( response => response.json() )
-        .then( data => console.log(data) )
+        .then( (data) => {
+            if( data.error ){
+                messageOne.textContent = data.error;
+            }else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        } )
 })
